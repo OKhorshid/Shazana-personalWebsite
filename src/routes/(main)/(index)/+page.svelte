ac8fdb5f42ea2e2f68a74d4ab4c8546367a679f6
Omar Khorshid <okhorshid.00@gmail.com>
2023-03-31 22:49:57 +0200

    add daisyUI

--- src/routes/(main)/(index)/+page.svelte
+++ src/routes/(main)/(index)/+page.svelte
@@ -1,4 +1,5 @@
 <section class="bg-blue-600">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<button class="btn btn-square btn-primary btn-wide">yooooo press meee</button>
 </section>
