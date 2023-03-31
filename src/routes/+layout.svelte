c2f40e4da39478f2ed80369e21e0083fb31ba693
Omar Khorshid <okhorshid.00@gmail.com>
2023-03-31 21:52:48 +0200

    add tailwind & error page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<layout class="bg-rose-400">
+	<slot />
+</layout>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="bg-blue-600">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</section>
